41b18f394bf08d3dc7ef4aef02a89155bbd39181
Jorge <keine1lust@gmail.com>
2021-06-05 23:03:45 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React, { useState } from 'react';
+import { TaskRow } from "./components/TaskRow";
+ 
 function App() {
+
+  const [taskItems, setTaskItems] = useState([
+    {name: 'Tasks One', done: false},
+    {name: 'Tasks Two', done: false},
+    {name: 'Tasks Three', done: true},      
+  ]);
+
+  const toggleTask = task =>
+    setTaskItems(
+      taskItems.map(t => (t.name === task.name ? { ...t, done: !t.done } : t))
+    );
+
+
+  const taskTableRow = () => 
+    taskItems.map(task => (
+      <TaskRow task={task} key={task.name} />
+    ))
+  
+
   return (
     <div className="App">
       <h1>Hello!! </h1>
+      <table>
+        <thead>
+          <tr> 
+            <td>Nombre</td>
+            <td>Status</td>
+          </tr>
+        </thead>
+        <tbody>{taskTableRow()}</tbody>
+      </table>
    </div>
   );
 }
